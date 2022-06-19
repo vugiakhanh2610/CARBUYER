7282947890268c4ba3f90f0ac33d13c27b5eb458
Vu Gia Khanh <khanh26102003a@gmail.com>
2022-06-19 12:19:29 +0700

    struggle to deploy to heroku

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-import uvicorn
+# import uvicorn
 
-if __name__ == "__main__":
-    uvicorn.run("server.app:app", host="localhost", port=8000, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("server.app:app", host="localhost", port=8000, reload=True)
